777d4a2868eff7f9b4bd5c12df15fee86a7715a0
Hidddlet <145039442+Hidddlet@users.noreply.github.com>
2023-09-29 06:13:21 +0300

    576

--- js/borger.js
+++ js/borger.js
@@ -1,8 +1,18 @@
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
+var acc = document.getElementsByClassName("header__icons");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active1");
+
+    /* Toggle between hiding and showing the active panel */
+    var nav__func = this.nextElementSibling;
+    if (nav__func.style.display === "block") {
+        nav__func.style.display = "none";
     } else {
-      x.style.display = "block";
+        nav__func.style.display = "block";
     }
+  });
 }
