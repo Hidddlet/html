1831c088dd2265c1e7f9953d05b77893421711dd
Hidddlet <145039442+Hidddlet@users.noreply.github.com>
2023-10-02 17:40:30 +0300

    fixed again

--- js/borger.js
+++ js/borger.js
@@ -1,6 +1,6 @@
 var menu = document.querySelector('.js-menu-button')
-var idk = document.querySelector('.js-header')
+var jsMenu = document.querySelector('.js-header')
 
 menu.addEventListener("click", function() {
-  idk.classList.toggle('active1');
+  jsMenu.classList.toggle('active1');
 });
